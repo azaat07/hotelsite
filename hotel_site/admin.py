99ed4888886f7bfbf2663e1077eb3900f3bedb15
azaat07 <sssma7583@gmail.com>
2024-10-21 19:34:35 +0600

    hello

--- hotel_site/admin.py
+++ hotel_site/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import *
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Hotel)
+admin.site.register(HotelImage)
+admin.site.register(Room)
+admin.site.register(ImageRoom)
+admin.site.register(Booking)
+
--- hotel_site/views.py
+++ hotel_site/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, status
+from .models import *
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class HotelViewSet(viewsets.ModelViewSet):
+    queryset = Hotel.objects.all()
+    serializer_class = HoterSerializer
+
+
+class HotelImageViewSet(viewsets.ModelViewSet):
+    queryset = HotelImage.objects.all()
+    serializer_class = HoterImageSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ImageRoomViewSet(viewsets.ModelViewSet):
+    queryset = ImageRoom.objects.all()
+    serializer_class = ImageRoomSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = HoterSerializer
